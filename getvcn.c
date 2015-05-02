15f35e88395cdb850c3653f38e6fd9d6d8fcc084
Enrico Polesel <saggiopol@gmail.com>
2015-05-02 17:48:18 +0200

    getvcn: rewrite using elib

--- getvcn.c
+++ getvcn.c
@@ -1,19 +1,22 @@
+#include <stdlib.h>
+#include <stdio.h>
+
 #include <net-snmp/net-snmp-config.h>
 #include <net-snmp/net-snmp-includes.h>
 
+#include "elib.h"
+
 int main(int argc, char** argv)
 {
-  struct snmp_session session; /* session: our target */ 
   struct snmp_session *ss; /*ss: returned by the library */
-  struct snmp_pdu *pdu; /* information to send to the host */
-  struct snmp_pdu *response; /* information from the host */
-
+  
   oid anOID[MAX_OID_LEN]; 
   size_t anOID_len = MAX_OID_LEN;
 
-  struct variable_list *vars; /* variables in the response */
-  int status;
+  struct snmp_pdu *response=NULL;
+  int status = -1;
   int member=-1;
+  int exit_code = 0;
   char* agentaddress=NULL;
   char* community=NULL;
 
@@ -46,118 +49,66 @@ int main(int argc, char** argv)
       }
   }   
 
+  status = elib_init(&ss, agentaddress, community );
+  switch (status)
+    {
+    case 0:
+      /* Connection opened */
+      break;
+    case 3:
+      /* Unable to open connection: UNKNOWN */
+      return 3;
+      break;
+    default:
+      /* A library bug */
+      printf ("Error in elib_init\n");
+      return 3;
+    }
   
-  /*
-   * Initialize the SNMP library
-   */
-  init_snmp("snmpapp");
+  /* Create the request */
 
-  /*
-   * Initialize a "session" that defines who we're going to talk to
-   */
-  snmp_sess_init( &session );                   /* set up defaults */
-  session.peername = agentaddress;
-
-  /* set the SNMP version number */
-  session.version = SNMP_VERSION_1;
-
-  /* set the SNMPv1 community name used for authentication */
-  session.community = community;
-  session.community_len = strlen(session.community);
-
-
-  /*
-   * Open the session
-   */
-  ss = snmp_open(&session);                     /* establish the session */
-
-  if (!ss) {
-    snmp_perror("ack");
-    printf("UNKNOWN - unable to connect to %s\n",session.peername);
-    exit(3);
-  }
-
-  /*
-   * Create the PDU for the data for our request.
-   *   1) We're going to GET the system.sysDescr.0 node.
-   */
-  pdu = snmp_pdu_create(SNMP_MSG_GET);
-  
+  /* query for jnxVirtualChassisMemberRole */
   read_objid("1.3.6.1.4.1.2636.3.40.1.4.1.1.1.3", anOID, &anOID_len);
+  /* jnxVirtualChassisMemberRole.member */
   anOID[anOID_len++] = member;
-  snmp_add_null_var(pdu, anOID, anOID_len);
-  
-  
-  /*
-   * Send the Request out.
-   */
-  status = snmp_synch_response(ss, pdu, &response);
-  
-  
-  /*
-   * Process the response.
-   */
-  if (status == STAT_SUCCESS && response->errstat == SNMP_ERR_NOERROR) {
-    /*
-     * SUCCESS: Print the result variables
-     */
-    if (response)
-      {
-	vars = response->variables;
-	printf("OK - ");
-	switch (*vars->val.integer)
-	  {
-	  case 1:
-	    printf("master");
-	    break;
-	  case 2:
-	    printf("backup");
-	    break;
-	  case 3:
-	    printf("linecard");
-	    break;
-	  }
-	printf("\n");
-	/*	print_variable(vars->name, vars->name_length, vars); */
-      }
-    else
-      {
-	printf("UNKNOWN - error in getting variables\n");
-	exit(3);
-      }
-    } else {
-    /*
-     * FAILURE: print what went wrong!
-     */
-    
-      if (status == STAT_SUCCESS)
+
+  status = elib_get_one_response(ss,&response, anOID, anOID_len, SNMP_MSG_GET  );
+
+  switch (status)
+    {
+    case 0:
+      /* OK with the query */
+      /* TODO: check that it really is a integer */
+      switch (*(response->variables->val.integer))
 	{
-	  if (response->errstat == SNMP_ERR_NOSUCHNAME)
-	    {
-	      printf("CRITICAL - member not found\n");
-	      exit(2);
-	    }
-	  else
-	    {
-	      printf("UNKNOWN - error in packet. Reason: %s\n",	snmp_errstring(response->errstat));
-	      exit(3);
-	    }
+	case 1:
+	  printf("OK - master\n");
+	  break;
+	case 2:
+	  printf("OK - backup\n");
+	  break;
+	case 3:
+	  printf("OK - linecard\n");
+	  break;
+	default:
+	  //	  printf("UNKNOWN - unknow jnxVirtualChassisMemberRole integer %ld for member %d\n", *var.val.integer, member);
+	  exit_code = 3;
+	  break;
 	}
-      else
-	{
-	  printf ("UNKNOWN - error in getting values\n");
-	  exit(3);
-	}
-  }
-  /*
-   * Clean up:
-   *  1) free the response.
-   *  2) close the session.
-   */
+      break;
+    case 2:
+      /* variable not found, so the member is not present */
+      printf("CRITICAL - member %d not found\n",member);
+      exit_code = 2;
+      break;
+    case 3:
+      exit_code = 3;
+    }
   if (response)
     {
       snmp_free_pdu(response);
     }
-  snmp_close(ss);
-  return 0;
+
+  status = elib_close(&ss);
+  return exit_code;
 }
