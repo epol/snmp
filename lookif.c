bc86bddc8a3b48a425c8c5b0f8565e7bef596f0e
Enrico Polesel <epol@autistici.org>
2015-05-02 18:23:54 +0200

    lookif: rewrite with elib

--- lookif.c
+++ lookif.c
@@ -5,83 +5,19 @@
 #include <net-snmp/net-snmp-config.h>
 #include <net-snmp/net-snmp-includes.h>
 
-void get_response(
-		  struct snmp_session *ss, /*ss: returned by the library */
-		  struct snmp_pdu **response, /* information from the host */
-		  oid anOID[],
-		  size_t anOID_len
-		  )
-{
-  struct snmp_pdu *pdu; /* information to send to the host */
-  int status;
-  /*
-   * Create the PDU for the data for our request.
-   *   1) We're going to GET the system.sysDescr.0 node.
-   */
-  pdu = snmp_pdu_create(SNMP_MSG_GET);
-  snmp_add_null_var(pdu, anOID, anOID_len);
-  
-  /*
-   * Send the Request out.
-   */
-  status = snmp_synch_response(ss, pdu, response);
-  
-  
-  /*
-   * Process the response.
-   */
-  if (status == STAT_SUCCESS && (*response)->errstat == SNMP_ERR_NOERROR) {
-    /*
-     * SUCCESS: Print the result variables
-     */
-    if (*response)
-      {
-	return;
-      }
-    else
-      {
-	printf("UNKNOWN - error in getting variables\n");
-	exit(3);
-      }
-    } else {
-    /*
-     * FAILURE: print what went wrong!
-     */
-    
-      if (status == STAT_SUCCESS)
-	{
-	  if ((*response)->errstat == SNMP_ERR_NOSUCHNAME)
-	    {
-	      printf("CRITICAL - variable not found\n");
-	      exit(2);
-	    }
-	  else
-	    {
-	      printf("UNKNOWN - error in packet. Reason: %s\n",	snmp_errstring((*response)->errstat));
-	      exit(3);
-	    }
-	}
-      else
-	{
-	  printf ("UNKNOWN - error in getting values\n");
-	  exit(3);
-	}
-  }
-}
 
 
 int main(int argc, char** argv)
 {
-  struct snmp_session session; /* session: our target */ 
-  struct snmp_session *ss; /*ss: returned by the library */
-  struct snmp_pdu *pdu; /* information to send to the host */
-  struct snmp_pdu *response; /* information from the host */
+  struct snmp_session *ss=NULL; /*ss: returned by the library */
+  struct snmp_pdu *response=NULL; /* information from the host */
 
   oid anOID[MAX_OID_LEN]; 
   size_t anOID_len = MAX_OID_LEN;
 
-  struct variable_list *vars; /* variables in the response */
-  int status;
+  int status=-1;
+  int exit_code=-1;
+  int found = 0;
   char* agentaddress=NULL;
   char* community=NULL;
   char* ifName=NULL;
@@ -127,155 +63,152 @@ int main(int argc, char** argv)
       }
   }   
 
-  
-  /*
-   * Initialize the SNMP library
-   */
-  init_snmp("snmpapp");
-
-  /*
-   * Initialize a "session" that defines who we're going to talk to
-   */
-  snmp_sess_init( &session );                   /* set up defaults */
-  session.peername = agentaddress;
-
-  /* set the SNMP version number */
-  session.version = SNMP_VERSION_2c;
-
-  /* set the SNMPv1 community name used for authentication */
-  session.community = community;
-  session.community_len = strlen(session.community);
-
-
-  /*
-   * Open the session
-   */
-  ss = snmp_open(&session);                     /* establish the session */
-
-  if (!ss) {
-    printf("UNKNOWN - unable to connect to %s\n",session.peername);
-    exit(3);
-  }
+  status = elib_init(&ss,agentaddress, community);
+  switch (status)
+    {
+    case 0:
+      /* Connection opened */
+      break;
+    case 3:
+      /* Unable to open connection: UNKNOWN */
+      return 3;
+      break;
+    default:
+      /* A library bug */
+      printf ("Error in elib_init\n");
+      return 3;
+    }
 
   /* 
    * Look for the interface name
    */
-  while(1)
-  {
-    pdu = snmp_pdu_create(SNMP_MSG_GETNEXT);
-
-    get_node("IF-MIB::ifName", anOID, &anOID_len);
-      //    read_objid("1.3.6.1.4.1.2636.3.40.1.4.1.1.1.3", anOID, &anOID_len);
-    anOID[anOID_len++] = ifIndex;
-    snmp_add_null_var(pdu, anOID, anOID_len);
-    
-    /*
-     * Send the Request out.
-     */
-    status = snmp_synch_response(ss, pdu, &response);
-    
-    
-    /*
-     * Process the response.
-     */
-    if (status == STAT_SUCCESS && response->errstat == SNMP_ERR_NOERROR) {
-      /*
-       * SUCCESS: Print the result variables
-       */
-      if (response)
+  found = 0;
+  while(!found)
+    {
+      get_node("IF-MIB::ifName", anOID, &anOID_len);
+      anOID[anOID_len++] = ifIndex;
+      
+      status = elib_get_one_response(ss,&response, anOID, anOID_len, SNMP_MSG_GETNEXT  );
+      
+      switch(status)
 	{
-	  vars = response->variables;
-	  if (vars->name[anOID_len-2]>1)
+	case 0:
+	  /* OK with the query */
+	  if (response->variables->name[anOID_len-2]>1)
+	    /* We are looking on the next table  */
 	    {
 	      printf("UNKNOWN - unable to find interface %s\n",ifName);
-	      exit(3);
+	      exit_code=3;
+	      found=1;
 	    }
 	  else
 	    {
-	      //	      print_variable(vars->name, vars->name_length, vars);
-	      //	      printf("%d %s\n", (vars->name)[anOID_len-1], vars->val.string);
-	      ifIndex = vars->name[anOID_len-1];
-	      if ( ( ifName_len == vars->val_len ) && (strncmp(vars->val.string, ifName, ifName_len) == 0 ))
+	      /*      print_variable(vars->name, vars->name_length, vars);*/
+	      ifIndex = response->variables->name[anOID_len-1];
+	      if ( ( ifName_len == response->variables->val_len ) && (strncmp(response->variables->val.string, ifName, ifName_len) == 0 ))
 		{
-		  break;
+		  exit_code=0;
+		  found = 1;
+		}
+	    }
+	  break;	
+	case 2:
+	case 3:
+	  printf("UNKNOWN - error while looking for the interface\n");
+	  exit_code=3;
+	  break;
+	default:
+	  printf("UNKNOWN - error in parsing status of elib_get_one_response\n");
+	  exit_code=3;
+	  break;
+	}
+    }
+  if (exit_code==0)
+    {
+      get_node("IF-MIB::ifOperStatus", anOID, &anOID_len);
+      anOID[anOID_len++] = ifIndex;
+      
+      status = elib_get_one_response(ss,&response, anOID, anOID_len, SNMP_MSG_GET  );
+      
+      switch(status)
+	{
+	case 0:
+	  /* OK with the query */
+	  ifStatus = 2 - *(response->variables->val.integer);
+	  exit_code=0;
+	  break;	
+	case 2:
+	case 3:
+	  printf("UNKNOWN - error while checking link status\n");
+	  exit_code=3;
+	  break;
+	default:
+	  printf("UNKNOWN - error in parsing status of elib_get_one_response\n");
+	  exit_code=3;
+	  break;
+	}
+      
+      if ( (exit_code==0) && ifStatus )
+	{
+	  get_node("IF-MIB::ifHighSpeed",anOID, &anOID_len);
+	  anOID[anOID_len++] = ifIndex;
+	  
+	  status = elib_get_one_response(ss,&response, anOID, anOID_len, SNMP_MSG_GET  );
+	  
+	  switch(status)
+	    {
+	    case 0:
+	      /* OK with the query */
+	      ifSpeed = *(response->variables->val.integer);
+	      exit_code=0;
+	      break;	
+	    case 2:
+	    case 3:
+	      printf("UNKNOWN - error while checking link speed\n");
+	      exit_code=3;
+	      break;
+	    default:
+	      printf("UNKNOWN - error in parsing status of elib_get_one_response\n");
+	      exit_code=3;
+	      break;
+	    }
+	}
+      
+      if ( exit_code==0 )
+	{
+	  if ( !ifStatus )
+	    {
+	      printf("CRITICAL - link %s is down\n",ifName);
+	      exit_code=2;
+	    }
+	  else
+	    {
+	      if ( ifSpeed < crit)
+		{
+		  printf("CRITICAL - link %s is up at %ld Mbit/s\n",ifName,ifSpeed);
+		  exit_code=2;
+		}
+	      else
+		{
+		  if ( ifSpeed < warn)
+		    {
+		      printf("WARNING - link %s is up at %ld Mbit/s\n",ifName,ifSpeed);
+		      exit_code=1;
+		    }
+		  else
+		    {
+		      printf ("OK - link %s is up at %ld Mbit/s\n",ifName,ifSpeed);
+		      exit_code=0;
+		    }
 		}
 	    }
 	}
-      else
-	{
-	  printf("UNKNOWN - Error in getting variables\n");
-	  exit(3);
-	}
-    } else {
-      /*
-       * FAILURE: print what went wrong!
-       */
-      
-      if (status == STAT_SUCCESS)
-	{
-	  printf("UNKNOWN - Error in packet. Reason: %s\n",
-		snmp_errstring(response->errstat));
-	  exit(3);
-	}
-      else
-	{
-	  printf("UNKNOWN - ");
-	  snmp_sess_perror("snmpget", ss);
-	  exit(3);
-	}
     }
-  }
-  //  printf("Index found: %d\n",ifIndex);
-
-  get_node("IF-MIB::ifOperStatus", anOID, &anOID_len);
-  anOID[anOID_len++] = ifIndex;
   
-  get_response(ss,&response,anOID,anOID_len);
-  vars = response->variables;
-  //  printf("%d\n",*(vars->val.integer));
-  ifStatus = 2 - *(vars->val.integer);
-
-  if ( ifStatus )
-    {
-      get_node("IF-MIB::ifHighSpeed",anOID, &anOID_len);
-      anOID[anOID_len++] = ifIndex;
-
-      get_response(ss,&response,anOID,anOID_len);
-      vars = response->variables;
-      //      printf("%d\n",*(vars->val.integer));
-      ifSpeed = *(vars->val.integer);
-    }
-
-  if ( !ifStatus )
-    {
-      printf("CRITICAL - link %s is down\n",ifName);
-      exit(2);
-    }
-  else
-    {
-      if ( ifSpeed < crit)
-	{
-	  printf("CRITICAL - link %s is up at %ld Mbit/s\n",ifName,ifSpeed);
-	  exit(2);
-	}
-      if ( ifSpeed < warn)
-	{
-	  printf("WARNING - link %s is up at %ld Mbit/s\n",ifName,ifSpeed);
-	  exit(1);
-	}
-      printf ("OK - link %s is up at %ld Mbit/s\n",ifName,ifSpeed);
-    }
-     
-  
-  /*
-   * Clean up:
-   *  1) free the response.
-   *  2) close the session.
-   */
   if (response)
     {
       snmp_free_pdu(response);
     }
-  snmp_close(ss);
-  return 0;
+  elib_close(ss);
+  return exit_code;
 }
