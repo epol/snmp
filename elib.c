0d0a3e4a62616741bdf1d15ea626b5018a6a74df
Enrico Polesel <epol@autistici.org>
2015-05-15 23:44:28 +0200

    addes license informations to sources

--- elib.c
+++ elib.c
@@ -1,3 +1,25 @@
+/*
+ * elib.c
+ * This file is part of epol/snmp
+ *
+ * Copyright (C) 2015 - Enrico Polesel
+ *
+ * epol/snmp is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * epol/snmp is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with epol/snmp. If not, see <http://www.gnu.org/licenses/>
+ * or write to the Free Software Foundation, Inc., 51 Franklin Street, 
+ * Fifth Floor, Boston, MA 02110-1301 USA.
+ */
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
