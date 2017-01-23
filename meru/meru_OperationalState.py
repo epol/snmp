3b5c24b11f71be7b1ab1b9fca7928e8762fcb904
Enrico Polesel <epol@autistici.org>
2017-01-23 18:02:28 +0100

    Meru AP checks
    - operationalState
    - availabilityStatus

---
+++ meru/meru_OperationalState.py
@@ -0,0 +1,77 @@
+#!/usr/bin/env python2
+# -*- coding: utf-8 -*-
+
+# meru_OperationalState.py
+# This file is part of epol/snmp
+#
+# Copyright (C) 2017 - Enrico Polesel
+#
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+
+import easysnmp
+import argparse
+import sys
+import json
+
+parser = argparse.ArgumentParser()
+parser.add_argument("-m", "--meruhost", help="Hostname of the meru controller",required=True)
+parser.add_argument("-C","-c", "--community", help="SNMP community of the meru controller",required=True)
+parser.add_argument("-i","--index",help="Index of the AP in the SNMP table",required=True,type=int)
+
+args = parser.parse_args()
+
+snmp_session = easysnmp.Session(hostname=args.meruhost,community=args.community, version=2)
+
+try:
+    desc = snmp_session.get(('.1.3.6.1.4.1.15983.1.1.4.2.1.1.2',args.index)).value
+    opstate = int(snmp_session.get(('.1.3.6.1.4.1.15983.1.1.4.2.1.1.26',args.index)).value)
+except:
+    print("UNKNOWN - Unable to get SNMP infos. Is the host reachable and the community correct?")
+    sys.exit(3)
+
+if opstate == 0:
+    state = 3
+    message = "operationalState is Unknown"
+elif opstate == 1:
+    state = 0
+    message = "operationalState is Enabled"
+elif opstate == 2:
+    state = 2
+    message = "operationalState is Disabled"
+elif opstate == 3:
+    state = 2
+    message = "operationalState is NoLicense"
+elif opstate == 4:
+    state = 1
+    message = "operationalState is EnabledWNLicense"
+elif opstate == 5:
+    state = 1
+    message = "operationalState is PowerDown"
+
+if state == 0:
+    statestr = "OK"
+elif state == 1:
+    statestr = "WARNING"
+elif state == 2:
+    statestr = "CRITICAL"
+elif state == 3:
+    statestr = "UNKNOWN"
+
+print ("{statestr} - {desc} {message}".format(statestr=statestr,desc=desc,message=message))
+sys.exit(state)
+
+
